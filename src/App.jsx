5a7dce176a5410cbf5cc987e923a83764d565b32
Santiago1025 <lusasantillanro@ittepic.edu.mx>
2024-11-15 13:03:01 -0700

    Fornulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import  Main from "./pages/main"
+import Formulario from "./pages/main/form";
 function App() {
   return (
     <>
       <Main/>
+      <Formulario/>
     </>
   );
 }
